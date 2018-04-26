87b5ca82991053eb01845da147bbc2829f2346ff
Otakukami <nikitapokidaylo@yahoo.com>
2018-04-26 16:39:07 -0700

    Initial commit

---
+++ src/FileMatchTest.java
@@ -0,0 +1,10 @@
+public class FileMatchTest {
+    public static void main( String args[] )
+{
+        FileMatch application = new FileMatch();
+
+        application.openFiles();
+        application.processFiles();
+        application.closeFiles();
+        }
+}
